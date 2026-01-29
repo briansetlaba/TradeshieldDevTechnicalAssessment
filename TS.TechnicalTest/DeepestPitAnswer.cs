c0b53d2aad6279d1073cbcfbd55184bd00a0251c
Brian Setlaba <briansetlaba@gmail.com>
2026-01-30 01:05:46 +0200

    now adding logic for input validation once more

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,9 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
+        if (points == null || points.Length < 3)
+        return -1;
+
         return -1;
     }
 }
