1ae2c7d99cdd1ee6e5ba17f6b10d018fb88c3771
Brian Setlaba <briansetlaba@gmail.com>
2026-02-02 11:02:14 +0200

    fixing errors ... urgh

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -1,62 +1,76 @@
 ﻿namespace TS.TechnicalTest;
 
-public class DeepestPitAnswer
-{
-    public static int Solution(int[] points)
+    public class DeepestPitAnswer
     {
-        if (points == null || points.Length < 3)
-        return -1;
-
-        int deepest = -1;
-        int index = 0;
-
-        while (index < points.Length - 1)
+        public static int Solution(int[] points)
         {
-            if (IsPitStart(points, index))
-            {
-                int p = index;
-                int q = FindValley(points, p);
-                int r = FindBridge(points, q);
+            if (!IsValid(points))
+                return -1;
 
-                if (isValidPit(p, q, r))
-                {
-                    deepest = Math.Min(deepest, CalculateDepth(points, p, q, r);
-                }
-                index = r;
-            }
-            else
+            int deepest = -1;
+
+            int index = 0;
+
+            while (index < points.Length - 1)
             {
-                   index++;
+                if (IsPitStart(points, index))
+                {
+                    int p = index;
+
+                    int q = FindValley(points, p);
+
+                    int r = FindRidge(points, q);
+
+                    if (IsValidPit(p, q, r))
+                    {
+                        deepest = Math.Max(deepest, CalculateDepth(points, p, q, r));
+                    }
+
+                    index = r;
+                }
+                else
+                {
+                    index++;
+                }
             }
+
+            return deepest;
         }
 
-        return deepest;
-    }
+     private static bool IsValid(int[] points)
+     => points != null && points.Length >= 3;
 
-    private static bool IsValid(int[] points) => points == null || points.Length < 3;
 
-    private static bool IsPitStart(int[] points, int index) => points[index] > 0 && points[index] > points[index + 1];
+    private static bool IsPitStart(int[] points, int index)
+     => points[index] > 0 && points[index] > points[index + 1];
+
 
     private static int FindValley(int[] points, int start)
     {
         int i = start + 1;
+
+        while (i + 1 < points.Length && points[i] > points[i + 1])
+            i++;
+
+        return i;
+    }
+
+    private static int FindRidge(int[] points, int valley)
+    {
+        int i = valley;
+
         while (i + 1 < points.Length && points[i] < points[i + 1])
             i++;
 
         return i;
     }
 
-    private static int FindBridge(int[] points, int valley)
-    {
-        int i = valley;
-
-        while (i + 1 < points.Length && points[i] < points[i + 1]) i++;
-        return i;
-    }
+    private static bool IsValidPit(int p, int q, int r)
+        => p < q && q < r;
 
 
-    private static bool isValidPit(int p, int q, int r) => p < q && q < r;
+    private static int CalculateDepth(int[] points, int p, int q, int r)
 
-    private static int CalculateDepth(int[] points, int p, int q, int r) => Math.Min(points[q], points[r]) - points[q])
-
+     => Math.Min(points[p] - points[q], points[r] - points[q]);
 }
+
